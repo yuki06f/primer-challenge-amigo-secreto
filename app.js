560356da4b506118283e7900ec31c7eb2e4dfbd1
fer hdz <ferhdz.06f@outlook.com>
2025-02-06 16:31:54 -0600

    codigo y challenge resuelto

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let listaAmigos = document.getElementById('listaAmigos');
+let input = document.getElementById('amigo');
+let totalAmigos = 0;
+let botonSorteo = document.querySelector('.button-draw');
+let botonAgregar = document.querySelector('.button-add');
+
+botonAgregar.addEventListener('click', (e) =>  {
+    if(amigos.length == 0){
+        listaAmigos.innerHTML = '';
+    }
+    
+    let amigo = input.value.trim();
+    if(amigo != '' && !amigos.includes(amigo)){
+        amigos.push(amigo);
+        nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = amigo;
+        listaAmigos.appendChild(nuevoElemento);
+        totalAmigos++;
+    }else if(amigos.includes(amigo)){ 
+        alert("No inserte el mismo nombre mas de una vez.");
+    }else{
+        alert("Ingresa un nombre valido.");
+    }
+    
+    input.value = '';
+
+});
+
+botonSorteo.addEventListener('click', (e) =>  {
+    if(amigos.length == 0){
+        alert("Comienza registrando amigos para sortear");
+        listaAmigos.innerHTML = '';
+    }else{
+        let numero, amigoSorteado;
+        numero = Math.floor(Math.random() * (totalAmigos));
+        amigoSorteado =  amigos[numero];
+        listaAmigos.innerHTML = `El amigo secreto es: ${amigoSorteado}`; 
+        amigos = [];
+        totalAmigos = 0;
+    }
+
+
+});
